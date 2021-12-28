398fdac540c594d10d483b367577a1c0071420db
Joris De Ridder <jorisderidder@outlook.be>
2021-12-28 15:15:36 +0100

    added storage import en pokemons aangepast

--- src/app/services/apicall.service.ts
+++ src/app/services/apicall.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map} from 'rxjs/operators';
+import { Storage } from '@capacitor/storage';
 
 
 @Injectable({
@@ -66,4 +67,6 @@ export class ApicallService {
      })
    );
  }
+
+
 }
