5489c07afeff18d1b67571d5ebd2790dc6cf5321
Joris De Ridder <jorisderidder@outlook.be>
2022-01-05 15:23:49 +0100

    name op homepage en randompage (before firebase)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CanvasDrawComponent } from './canvas-draw/canvas-draw.component';
 import { CommonModule } from '@angular/common';
+
 @NgModule({
   declarations: [AppComponent, CanvasDrawComponent],
   entryComponents: [],
