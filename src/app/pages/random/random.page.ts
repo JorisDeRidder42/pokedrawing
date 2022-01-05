f7d87d9e1aab8a8a45747e8c78cc109b63f821de
Joris De Ridder <jorisderidder@outlook.be>
2022-01-05 18:05:35 +0100

    added pokemonname on homempage and randompage

--- src/app/pages/random/random.page.ts
+++ src/app/pages/random/random.page.ts
@@ -12,17 +12,20 @@ export class RandomPage implements OnInit {
   afbeelding: string;
   indexnummerpokemon: number;
   gekniptelink: string;
+  pokemon1:any;
   
   ngOnInit(){
     this.LoadImagePokemon();
   }
   async LoadImagePokemon(){
     this.knipUrl();
+    this.getPokemonNameLikeHomePage();
   }
 
   randomPokemon(){
     this.afbeelding = this.apiService.getPokeImage(this.apiService.CreateRandomIndex());
     this.knipUrl();
+    this.getPokemonNameLikeHomePage();
   }
 
   knipUrl(){
@@ -30,6 +33,11 @@ export class RandomPage implements OnInit {
   this.afbeelding = this.apiService.getPokeImage(b);
   //console.log(this.afbeelding);
   this.gekniptelink = this.afbeelding.substring(this.afbeelding.lastIndexOf("/") + 1,this.afbeelding.lastIndexOf("."));
+  }
+  getPokemonNameLikeHomePage(){
+    this.apiService.getPokemon1(this.gekniptelink).subscribe(res => {
+      this.pokemon1 = res;
+    });
   }
 
 }
